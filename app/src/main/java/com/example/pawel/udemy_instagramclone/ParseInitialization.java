5d67883fedd386e7833549b46ddc09bb384e6049
pawelsa <pawel.sapek97@gmail.com>
2018-05-17 16:31:36 +0200

    Update ParseInitialization.java

--- app/src/main/java/com/example/pawel/udemy_instagramclone/ParseInitialization.java
+++ app/src/main/java/com/example/pawel/udemy_instagramclone/ParseInitialization.java
@@ -3,4 +3,28 @@ package com.example.pawel.udemy_instagramclone;
 import android.app.Application;
 
 public class ParseInitialization extends Application {
+  
+  @Override
+	public void onCreate() {
+		super.onCreate();
+		parseInitialization();
+	}
+
+	private void parseInitialization() {
+
+		Parse.enableLocalDatastore(getApplicationContext());
+
+		// Add your initialization code here
+		Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+				.applicationId("8c6f3c6c1bb88c1f3a6bc607368019f43db8ce1b")
+				.clientKey("c8d8271fb51a5aaf7bfd38d6976666755b626665")
+				.server("http://13.58.211.243:80/parse/")
+				.build()
+		);
+		ParseACL defaultACL = new ParseACL();
+		defaultACL.setPublicReadAccess(true);
+		defaultACL.setPublicWriteAccess(true);
+		ParseACL.setDefaultACL(defaultACL, true);
+
+	}
 }
